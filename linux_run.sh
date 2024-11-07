2d7a0b5dd8fbaf2bb1cba031c1a0e41c2ecf3434
Nobpadon Klaypan <77038173+Owlread@users.noreply.github.com>
2024-11-07 14:44:29 +0700

    Delete linux_run.sh

--- linux_run.sh
+++
@@ -1,9 +0,0 @@
-[Desktop Entry]
-Name=Bell's Palsy Application
-Comment=Run Bell's Palsy Application
-Exec=python3 /home/nobpadon/Desktop/Dataapp/Bells_Palsy.py
-Icon=utilities-terminal
-Terminal=true
-Type=Application
-
-chmod +x ~/Desktop/run_app.desktop
