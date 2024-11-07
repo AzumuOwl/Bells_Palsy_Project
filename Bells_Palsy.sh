bbcec230bb1dd6060cd098033de9f5d3a17b4844
Nobpadon Klaypan <77038173+Owlread@users.noreply.github.com>
2024-11-07 10:29:09 +0700

    Delete Bells_Palsy.sh

--- Bells_Palsy.sh
+++
@@ -1,9 +0,0 @@
-chmod +x ~/Desktop/run_app.desktop
-
-[Desktop Entry]
-Name=Bell's Palsy Application
-Comment=Run Bell's Palsy Application
-Exec=python3 /home/pi/Desktop/Dataapp/Bells_Palsy.py
-Icon=utilities-terminal
-Terminal=true
-Type=Application
