ecdaf5d0dbe5c6ef2a2ba37da812eaf83fbfb581
Nobpadon Klaypan <77038173+Owlread@users.noreply.github.com>
2024-11-07 10:17:54 +0700

    Update Bells_Palsy.sh

--- Bells_Palsy.sh
+++ Bells_Palsy.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-echo "Starting the Bell's Palsy application..."
-python3 Bells_Palsy.py
-read -p "Press [Enter] key to continue..."
-
-----
-chmod +x run.sh
-
-chmod +x /home/pi/run.sh
 chmod +x ~/Desktop/run_app.desktop
+
+[Desktop Entry]
+Name=Bell's Palsy Application
+Comment=Run Bell's Palsy Application
+Exec=python3 /home/pi/Desktop/Dataapp/Bells_Palsy.py
+Icon=utilities-terminal
+Terminal=true
+Type=Application
