df9bbc1c4ecf8967d9fd2eea6f057b85a06aeb6a
Nobpadon Klaypan <77038173+Owlread@users.noreply.github.com>
2024-11-07 10:29:53 +0700

    Update linux_run.sh

--- linux_run.sh
+++ linux_run.sh
@@ -1,7 +1,7 @@
 [Desktop Entry]
 Name=Bell's Palsy Application
 Comment=Run Bell's Palsy Application
-Exec=/home/pi/run.sh
+Exec=python3 /home/pi/Desktop/Dataapp/Bells_Palsy.py
 Icon=utilities-terminal
 Terminal=true
 Type=Application
